9fd5a46dcf1a66f24f37b47619835421f4aca226
vishesh2539 <123531844+vishesh2539@users.noreply.github.com>
2023-03-23 12:07:10 +0530

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -38,3 +38,54 @@ def gameLoop():
     foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
     clock = pygame.time.Clock()
+    # Game loop flag
+    game_over = False
+
+    while not game_over:
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block_size
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block_size
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block_size
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block_size
+                    x1_change = 0
+
+        # Check if the snake hits the boundaries
+        if x1 >= window_width or x1 < 0 or y1 >= window_height or y1 < 0:
+            game_over = True
+
+        # Update the snake position
+        x1 += x1_change
+        y1 += y1_change
+
+        # Draw the snake and the food
+        game_display.fill(black)
+        pygame.draw.rect(game_display, red, [food_x, food_y, snake_block_size, snake_block_size])
+        pygame.draw.rect(game_display, white, [x1, y1, snake_block_size, snake_block_size])
+        pygame.display.update()
+
+        # Check if the snake eats the food
+        if x1 == food_x and y1 == food_y:
+            print("Yummy!!")
+            food_x = round(random.randrange(0, window_width - snake_block_size) / 10.0) * 10.0
+            food_y = round(random.randrange(0, window_height - snake_block_size) / 10.0) * 10.0
+
+        # FPS
+        clock = pygame.time.Clock()
+        clock.tick(snake_speed)
+
+    # Quit pygame
+    pygame.quit()
+    quit()
+
+game_loop()
